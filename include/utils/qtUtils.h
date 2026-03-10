7d274f34bd4c2aaee5568ae23fec50a71fab9735
ZHOU <z18917941512@qq.com>
2026-03-10 15:34:52 +0800

    ready to test ring buffer

--- include/utils/qtUtils.h
+++ include/utils/qtUtils.h
@@ -1,7 +1,7 @@
 #ifndef UNICOMM_QTUTILS_H
 #define UNICOMM_QTUTILS_H
 
-#include <QByteArray>
+#include<QMutexLocker>
 
 class QUrl;
 
@@ -10,5 +10,26 @@ QByteArray fileHashCalc(const QString &fileInfo);
 QByteArray fileHashCalc(const QUrl &fileInfo);
 
 QString md2plain(const QString &markdown);
+
+class RingBuffer final {
+public:
+    explicit RingBuffer(qsizetype capacity);
+
+    [[nodiscard]] qsizetype write(const QByteArray &data);
+
+    [[nodiscard]] QByteArray read(qsizetype length);
+
+    [[nodiscard]] qsizetype used();
+
+    void clear();
+
+private:
+    const qsizetype m_capacity{};
+    QByteArray m_buffer{};
+    qsizetype m_readPos{};
+    qsizetype m_writePos{};
+    qsizetype m_used{};
+    QMutex m_mutex{};
+};
 
 #endif //UNICOMM_QTUTILS_H
--- src/utils/qtUtils.cpp
+++ src/utils/qtUtils.cpp
@@ -8,12 +8,12 @@
 QByteArray fileHashCalc(const QString &fileInfo) {
     QFile file(fileInfo);
     if (!file.open(QIODevice::ReadOnly)) {
-        return QByteArray();
+        return {};
     }
     if (QCryptographicHash hash(QCryptographicHash::Sha256); hash.addData(&file)) {
         return hash.result();
     }
-    return QByteArray();
+    return {};
 }
 
 QByteArray fileHashCalc(const QUrl &fileInfo) {
@@ -26,4 +26,53 @@ QString md2plain(const QString &markdown) {
     doc.setMarkdown(markdown.toHtmlEscaped());
     const QString plain = doc.toPlainText();
     return plain;
+}
+
+// public
+RingBuffer::RingBuffer(const qsizetype capacity)
+    : m_capacity(capacity),
+      m_buffer(m_capacity, 0) {
+}
+
+qsizetype RingBuffer::write(const QByteArray &data) {
+    QMutexLocker locker(&m_mutex);
+    const qsizetype length = data.size();
+    if (length == 0 || length > m_capacity - m_used) return 0;
+    const qsizetype firstChunk = qMin(length, m_capacity - m_writePos);
+    const qsizetype secondChunk = length - firstChunk;
+    if (firstChunk > 0) m_buffer.replace(m_writePos, firstChunk, data.constData(), firstChunk);
+    if (secondChunk > 0) m_buffer.replace(static_cast<qsizetype>(0), secondChunk, data.constData() + firstChunk, secondChunk);
+
+    m_writePos = (m_writePos + length) % m_capacity;
+    m_used += length;
+    return length;
+}
+
+QByteArray RingBuffer::read(qsizetype length) {
+    QMutexLocker locker(&m_mutex);
+    if (length < 0 || m_used == 0) return {};
+    if (length == 0 || length > m_used) length = m_used;
+    QByteArray data;
+    data.reserve(length);
+    const qsizetype firstChunk = qMin(length, m_capacity - m_readPos);
+    const qsizetype secondChunk = length - firstChunk;
+    if (firstChunk > 0) data.append(m_buffer.constData() + m_readPos, firstChunk);
+    if (secondChunk > 0) data.append(m_buffer.constData(), secondChunk);
+
+    m_readPos = (m_readPos + length) % m_capacity;
+    m_used -= length;
+    return data;
+}
+
+qsizetype RingBuffer::used() {
+    QMutexLocker locker(&m_mutex);
+    return m_used;
+}
+
+void RingBuffer::clear() {
+    QMutexLocker locker(&m_mutex);
+    m_buffer.fill(0);
+    m_readPos = 0;
+    m_writePos = 0;
+    m_used = 0;
 }
