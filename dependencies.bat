5a4ab81e7b368e3eba42f5ad8e2ceb4f7f8aa853
ZHOU <z18917941512@qq.com>
2026-06-07 15:56:43 +0800

    installed finished

--- dependencies.bat
+++ dependencies.bat
@@ -2,12 +2,22 @@
 setlocal enabledelayedexpansion
 
 set "EXE_PATH=cmake-build-debug-ucrt64\UniComm.exe"
+set "PLUGIN_PATH=cmake-build-debug-ucrt64\share\qt6\plugins\multimedia\ffmpegmediaplugin.dll"
 set "MSYS2_PATH=C:\msys64\ucrt64\bin"
+set "PATH=%MSYS2_PATH%;%PATH%"
 set "OUTPUT_PATH=3rd\misc\bin"
 
 echo Checking for missing MSYS2 DLLs...
 
 for /f "tokens=*" %%i in ('ntldd -R "%EXE_PATH%" ^| find "%MSYS2_PATH%"') do (
+    for /f "tokens=3 delims=> " %%j in ("%%i") do (
+        set "dll_path=%%j"
+        echo !dll_path! copied
+        copy "!dll_path!" "%OUTPUT_PATH%" >nul
+    )
+)
+
+for /f "tokens=*" %%i in ('ntldd -R "%PLUGIN_PATH%" ^| find "%MSYS2_PATH%"') do (
     for /f "tokens=3 delims=> " %%j in ("%%i") do (
         set "dll_path=%%j"
         echo !dll_path! copied
