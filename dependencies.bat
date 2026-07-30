5d4729037f680e7a104d44a56bcab039381f01a4
ZHOU <z18917941512@qq.com>
2026-07-30 12:53:30 +0800

    sqlite successfully installed

--- dependencies.bat
+++ dependencies.bat
@@ -1,8 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "EXE_PATH=cmake-build-debug-ucrt64\UniComm.exe"
-set "PLUGIN_PATH=cmake-build-debug-ucrt64\share\qt6\plugins\multimedia\ffmpegmediaplugin.dll"
+set "EXE_PATH=cmake-build-debug\UniComm.exe"
+set "FFMPEG_PATH=cmake-build-debug\share\qt6\plugins\multimedia\ffmpegmediaplugin.dll"
+set "SQLITE_PATH=cmake-build-debug\share\qt6\plugins\sqldrivers\qsqlite.dll"
 set "MSYS2_PATH=C:\msys64\ucrt64\bin"
 set "PATH=%MSYS2_PATH%;%PATH%"
 set "OUTPUT_PATH=3rd\misc\bin"
@@ -17,7 +18,15 @@ for /f "tokens=*" %%i in ('ntldd -R "%EXE_PATH%" ^| find "%MSYS2_PATH%"') do (
     )
 )
 
-for /f "tokens=*" %%i in ('ntldd -R "%PLUGIN_PATH%" ^| find "%MSYS2_PATH%"') do (
+for /f "tokens=*" %%i in ('ntldd -R "%FFMPEG_PATH%" ^| find "%MSYS2_PATH%"') do (
+    for /f "tokens=3 delims=> " %%j in ("%%i") do (
+        set "dll_path=%%j"
+        echo !dll_path! copied
+        copy "!dll_path!" "%OUTPUT_PATH%" >nul
+    )
+)
+
+for /f "tokens=*" %%i in ('ntldd -R "%SQLITE_PATH%" ^| find "%MSYS2_PATH%"') do (
     for /f "tokens=3 delims=> " %%j in ("%%i") do (
         set "dll_path=%%j"
         echo !dll_path! copied
