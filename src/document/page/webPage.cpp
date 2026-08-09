326bc64daee07f6ff2f65cc88a206fe78212c786
ZHOU <z18917941512@qq.com>
2026-08-09 19:09:40 +0800

    webview page name & icon rewritten

--- src/document/page/webPage.cpp
+++ src/document/page/webPage.cpp
@@ -1,11 +1,15 @@
 #include "document/page/webPage.h"
 
+#include <QIcon>
+
 #include "document/module/webviewWidget.h"
 
 // public
 WebPage::WebPage(const QJsonObject &documentConfig, const QUrl &documentUrl)
     : DocumentPage(documentUrl),
       m_webviewWidget(new WebviewWidget()) {
+    setTitle(documentUrl.host());
+    setIcon(QIcon(":/icon/fileTypeHttp.svg"));
     setWidget(m_webviewWidget);
     m_webviewWidget->navigate(documentUrl);
 }
